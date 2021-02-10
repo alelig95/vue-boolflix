e3165967509c346c0c71d090c7c39a87bdafd2cb
Alessandro Liggieri <smmb@live.it>
2021-02-10 15:06:23 +0100

    Milestone 2 in corso

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ new Vue ({
     el: '#app',
     data: {
         movieList: [],
+        tvList: [],
+        averageVoteList: [1, 2, 3, 4, 5],
         searchInput: ''
     },
     methods: {
@@ -10,8 +12,23 @@ new Vue ({
             axios.get('https://api.themoviedb.org/3/search/movie?api_key=8f64355215adbb41301dba970e421acd&query=' + self.searchInput)
             .then(function(resp) {
                 self.movieList = resp.data.results;
+                
             });
+            axios.get('https://api.themoviedb.org/3/search/tv?api_key=8f64355215adbb41301dba970e421acd&query=' + self.searchInput)
+            .then(function(resp) {
+                self.tvList = resp.data.results;
+                
+            });
+        },
+        coloredStars: function(element, index) {
+            const averageVote = Math.ceil(element.vote_average/2);
+              if (index + 1 <= averageVote) {
+                  return 'colored-star'
+              } else {
+                  return 'white'
+              }
         }
+    
     }
 });
 Vue.config.devtools = true;
