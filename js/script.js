fc462186164f12fc52899cce83ccfbe7c914c9ad
Alessandro Liggieri <smmb@live.it>
2021-02-11 17:13:19 +0100

    Milestone 4 completa

--- js/script.js
+++ js/script.js
@@ -1,39 +1,42 @@
 new Vue ({
     el: '#app',
     data: {
-        movieList: [],
-        tvList: [],
-        flagList:['en', 'it'],
+        moviesList: [],
+        tvSeriesList: [],
+        flagsList:['en', 'it', 'de', 'es', 'ja', 'pl', 'fr'],
         searchInput: ''
     },
     methods: {
-        search: function() {
-            this.searchMovie()
-            this.searchTvSeries()
-        },
+        /* Creo un metodo per popolare l'array dei film: */
         searchMovie: function() {
             const self = this;
             axios.get('https://api.themoviedb.org/3/search/movie?api_key=8f64355215adbb41301dba970e421acd&query=' + self.searchInput)
             .then(function(resp) {
-                self.movieList = resp.data.results;
+                self.moviesList = resp.data.results;
             });
         },
+        /* Creo un metodo per popolare l'array delle serie TV: */
         searchTvSeries: function() {
             const self = this;
             axios.get('https://api.themoviedb.org/3/search/tv?api_key=8f64355215adbb41301dba970e421acd&query=' + self.searchInput)
             .then(function(resp) {
-                self.tvList = resp.data.results;
+                self.tvSeriesList = resp.data.results;
             });
         },
-        coloredStars: function(element, index) {
+        /* Unisco in un unico metodo i due metodi creati in precedenza: */
+        search: function() {
+            this.searchMovie()
+            this.searchTvSeries()
+        },
+        /* Creo un metodo per dare la valutazione media sottoforma di stelle: */
+        ratingStars: function(element, index) {
             const averageVote = Math.ceil(element.vote_average/2);
               if (index + 1 <= averageVote) {
-                  return 'colored-star'
+                  return 'golden-star'
               } else {
-                  return 'white'
+                  return 'white-star'
               }
-        }
-    
+        }   
     }
 });
 Vue.config.devtools = true;
